a1f9dd61949233dd4256fbac134ad74a2e7b818f
Lugga <lucakruczinski@gmail.com>
2025-01-28 15:24:17 +0100

    Added HUD and functions to spawn mobs

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,9 +13,44 @@ func _process(delta: float) -> void:
 
 
 func game_over() -> void:
-	pass # Replace with function body.
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	mob.position = mob_spawn_location.position
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150,250), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+
+
+func _on_score_timer_timeout() -> void:
+	score +=1
+	$HUD.update_score(score)
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
